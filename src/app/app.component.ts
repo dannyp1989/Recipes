f5c90ec3e09cb50476d0c2e65bedf92604a80e58
dannyp1989 <danielpasak@gmail.com>
2017-12-27 23:52:19 +0100

    recipe-list edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showRecipes = 'recipe';
+  onNavChange(features: string) {
+    console.log('app get: ' + features);
+    if ( features === 'recipe') {
+      this.showRecipes = 'recipe';
+    } else {
+      this.showRecipes = 'shopping';
+    }
+  }
+
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+
+  @Output()
+  navChange = new EventEmitter<string>();
+
+  onNavClick(features: string) {
+    this.navChange.emit(features);
+    console.log('Header send: ' + features);
+  }
 
 }
